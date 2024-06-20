8bece59e7c303e4b0ec93ee428ed335226e6bd38
jbinyim <jbinyim12@naver.com>
2024-06-20 22:46:40 +0900

    240620 header작업중

--- netflex/src/components/Header.tsx
+++ netflex/src/components/Header.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faSortDown } from "@fortawesome/free-solid-svg-icons";
+import { faBell } from "@fortawesome/free-regular-svg-icons";
 
 const HeaderBox = styled.div`
   width: 100%;
   height: 70px;
   display: flex;
+  justify-content: space-between;
 `;
 
 const MenuBox = styled.div`
@@ -14,7 +18,7 @@ const MenuBox = styled.div`
   gap: 45px;
 `;
 
-const Img = styled.img`
+const Logo = styled.img`
   width: 92px;
   height: 50px;
   cursor: pointer;
@@ -34,18 +38,80 @@ const Menu = styled.li`
   }
 `;
 
+const SearcMenuhBox = styled.div`
+  margin-right: 60px;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  .fa-bell {
+    font-size: 20px;
+    cursor: pointer;
+  }
+`;
+
+const SearchBox = styled.div`
+  width: 275px;
+  display: flex;
+  gap: 10px;
+  padding: 5px;
+  border: 1px solid #fff;
+  .fa-magnifying-glass {
+    font-size: 22px;
+    cursor: pointer;
+  }
+`;
+
+const Search = styled.input`
+  width: 100%;
+  height: 22px;
+  background: transparent;
+  border: none;
+`;
+
+const ProfileBox = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  .fa-chevron-down {
+    font-size: 10px;
+  }
+`;
+
+const Profile = styled.img`
+  height: 32px;
+  border-radius: 3px;
+`;
+
 const Header = () => {
   return (
     <HeaderBox>
       <MenuBox>
-        <Img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/netflix_logo_icon_170918.png" />
+        <Logo
+          src="https://cdn.icon-icons.com/icons2/2699/PNG/512/netflix_logo_icon_170918.png"
+          alt="logo"
+        />
         <MenuList>
           <Menu>홈</Menu>
           <Menu>시리즈</Menu>
           <Menu>영화</Menu>
         </MenuList>
       </MenuBox>
-      <div></div>
+      <SearcMenuhBox>
+        <SearchBox>
+          <FontAwesomeIcon icon={faSearch} />
+          <Search type="text" placeholder="제목, 사람, 장르" />
+        </SearchBox>
+        <p>키즈</p>
+        <FontAwesomeIcon icon={faBell} />
+        <ProfileBox>
+          <Profile
+            src="https://i.pinimg.com/474x/61/54/76/61547625e01d8daf941aae3ffb37f653.jpg"
+            alt="profile"
+          />
+          <FontAwesomeIcon icon={faSortDown} />
+        </ProfileBox>
+      </SearcMenuhBox>
     </HeaderBox>
   );
 };
